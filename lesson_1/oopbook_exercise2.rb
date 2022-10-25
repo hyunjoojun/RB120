9849ba550f218345ad29e53856d9b9224947fffa
Hyunjoo Jun <hjj102088@gmail.com>
2022-10-25 16:34:12 -0400

    Car exercise

--- lesson_1/oopbook_exercise2.rb
+++ lesson_1/oopbook_exercise2.rb
@@ -1,16 +1,30 @@
-class MyCar
+module Towable
+  def can_tow?(pounds)
+    pounds < 2000
+  end
+end
+
+class Vehicle
   attr_accessor :color
   attr_reader :year, :model
+
+  @@number_of_vehicles = 0
+
+  def self.number_of_vehicles
+    puts "This program has created #{@@number_of_vehicles} vehicles"
+  end
 
   def self.gas_mileage(gallons, miles)
     puts "#{miles / gallons} miles per gallon of gas"
   end
 
-  def initialize(year, color, model)
+  def initialize(type, year, color, model)
+    @type = type
     @year = year
     @color = color
     @model = model
     @current_speed = 0
+    @@number_of_vehicles += 1
   end
 
   def speed_up(number)
@@ -38,9 +52,30 @@ class MyCar
   end
 
   def to_s
-    "My car is a #{color} #{year} #{model}!"
+    "My #{@type} is a #{color} #{year} #{model}!"
+  end
+
+  def age
+    "Your #{model} is #{years_old} years old."
+  end
+
+  private
+
+  def years_old
+    Time.now.year - self.year.to_i
   end
 end
 
-my_car = MyCar.new('2016', 'white', 'Honda Accord')
+class MyTruck < Vehicle
+  include Towable
+  NUMBER_OF_DOORS = 2
+end
+
+class MyCar < Vehicle
+  NUMBER_OF_DOORS = 4
+end
+
+my_car = MyCar.new('Car', '2016', 'white', 'Honda Accord')
+my_truck = MyTruck.new('Truck', '2019', 'black', 'Nissan Frontier')
+puts my_car.age
 puts my_car
