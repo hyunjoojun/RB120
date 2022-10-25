25576810ab3e0fc2d2e459227e61eef1e08a9b34
Hyunjoo Jun <hjj102088@gmail.com>
2022-10-25 16:47:40 -0400

    More exercise

--- lesson_1/oopbook_exercise3.rb
+++ lesson_1/oopbook_exercise3.rb
@@ -1,12 +1,44 @@
+class Student
+  attr_accessor :name, :grade
+
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
+
+  def better_grade_than?(other_student)
+    grade > other_student.grade
+  end
+
+  protected
+
+  def grade
+    @grade
+  end
+end
+
+joe = Student.new('Joe', 90)
+bob = Student.new('Bob', 84)
+puts "Well done!" if joe.better_grade_than?(bob)
+
+
 class Person
   attr_accessor :name
 
   def initialize(name)
     @name = name
   end
+
+  def public_hi
+    hi
+  end
+
+  private
+
+  def hi
+    "hi"
+  end
 end
 
 bob = Person.new('Steve')
-puts bob.name
-bob.name = 'Bob'
-puts bob.name
+puts bob.public_hi
