f09ed2ac40e84fd5b75f6e5e9fe4e1542a47ef44
Hyunjoo Jun <hjj102088@gmail.com>
2022-11-07 16:17:04 -0500

    Easy1 Q3

---
+++ lesson_4/easy1/q3.rb
@@ -0,0 +1,19 @@
+module Speed
+  def go_fast
+    puts "I am a #{self.class} and going super fast!"
+  end
+end
+
+class Car
+  include Speed
+  def go_slow
+    puts "I am safe and driving slow."
+  end
+end
+
+small_car = Car.new
+small_car.go_fast
+
+# We have a Car object that's referenced by small_car variable.
+# We call go_fast method on the small_car object and this prints out the string.
+# self.class is small_car's class which is Car so Car gets printed out.
