b4970884246c53195bf4ac896d1bd63e1da9047a
Hyunjoo Jun <hjj102088@gmail.com>
2022-11-09 15:25:29 -0500

    rubocop

--- lesson_5/ttt.rb
+++ lesson_5/ttt.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Board
   WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                   [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
@@ -18,19 +16,23 @@ class Board
     @squares.keys.select { |key| @squares[key].unmarked? }
   end
 
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/MethodLength
   def draw
-    puts "     |     |"
+    puts '     |     |'
     puts "  #{@squares[1]}  |  #{@squares[2]}  |  #{@squares[3]}"
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
+    puts '     |     |'
+    puts '-----+-----+-----'
+    puts '     |     |'
     puts "  #{@squares[4]}  |  #{@squares[5]}  |  #{@squares[6]}"
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
+    puts '     |     |'
+    puts '-----+-----+-----'
+    puts '     |     |'
     puts "  #{@squares[7]}  |  #{@squares[8]}  |  #{@squares[9]}"
-    puts "     |     |"
+    puts '     |     |'
   end
+  # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Metrics/MethodLength
 
   def full?
     unmarked_keys.empty?
@@ -57,13 +59,12 @@ class Board
   def three_identical_markers?(squares)
     markers = squares.select(&:marked?).collect(&:marker)
     return false if markers.size != 3
-
     markers.min == markers.max
   end
 end
 
 class Square
-  INITIAL_MARKER = " "
+  INITIAL_MARKER = ' '
 
   attr_accessor :marker
 
@@ -106,45 +107,46 @@ class TTTGame
     @current_marker = FIRST_TO_MOVE
   end
 
-  def play
-    clear
-    display_welcome_message
+  def player_move
+    current_player_moves
+    break if board.someone_won? || board.full?
+    clear_screen_and_display_board if human_turn?
+  end
 
+  def main_game
     loop do
       display_board
-
-      loop do
-        current_player_moves
-        break if board.someone_won? || board.full?
-
-        clear_screen_and_display_board if human_turn?
-      end
+      player_move
       display_result
       break unless play_again?
-
       reset
       display_play_again_message
     end
+  end
 
+  def play
+    clear
+    display_welcome_message
+    main_game
     display_goodbye_message
   end
 
   private
 
   def display_welcome_message
-    puts "Welcome to Tic Tac Toe!"
-    puts ""
+    puts 'Welcome to Tic Tac Toe!'
+    puts ''
   end
 
   def display_goodbye_message
-    puts "Thanks for playing Tic Tac Toe! Goodbye!"
+    puts 'Thanks for playing Tic Tac Toe! Goodbye!'
   end
 
   def display_board
     puts "You're a #{human.marker}. Computer is a #{computer.marker}."
-    puts ""
+    puts ''
     board.draw
-    puts ""
+    puts ''
   end
 
   def clear_screen_and_display_board
@@ -187,9 +189,9 @@ class TTTGame
 
     case board.winning_marker
     when human.marker
-      puts "You won!"
+      puts 'You won!'
     when computer.marker
-      puts "Computer won!"
+      puts 'Computer won!'
     else
       puts "It's a tie!"
     end
@@ -198,10 +200,11 @@ class TTTGame
   def play_again?
     answer = nil
     loop do
-      puts "Would you like to play again? (y/n)"
+      puts 'Would you like to play again? (y/n)'
       answer = gets.chomp.downcase
-      break if %w(y n).include? answer
-      puts "Sorry, must be y or n"
+      break if %w(y n).include?(answer)
+
+      puts 'Sorry, must be y or n'
     end
     answer == 'y'
   end
