5ea3cccffcd933e0f76ae0a0becd97800f712fcd
Hyunjoo Jun <hjj102088@gmail.com>
2022-11-09 11:18:46 -0500

    Improvements

--- lesson_5/ttt.rb
+++ lesson_5/ttt.rb
@@ -27,7 +27,7 @@ class Board
   end
 
   def someone_won?
-    !!detect_winner
+    !!winning_marker
   end
 
   def count_human_marker(squares)
@@ -39,7 +39,7 @@ class Board
   end
 
   # returns winning marker or nil
-  def detect_winner
+  def winning_marker
     WINNING_LINES.each do |line|
       if count_human_marker(@squares.values_at(*line)) == 3
         return TTTGame::HUMAN_MARKER
@@ -102,8 +102,7 @@ class TTTGame
     puts "Thanks for playing Tic Tac Toe! Goodbye!"
   end
 
-  def display_board(clear_screen = true)
-    clear if clear_screen
+  def display_board
     puts "You're a #{human.marker}. Computer is a #{computer.marker}."
     puts ""
     puts "     |     |"
@@ -118,6 +117,11 @@ class TTTGame
     puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
     puts "     |     |"
     puts ""
+  end
+
+  def clear_screen_and_display_board
+    clear
+    display_board
   end
 
   def human_moves
@@ -136,9 +140,9 @@ class TTTGame
   end
 
   def display_result
-    display_board
+    clear_screen_and_display_board
 
-    case board.detect_winner
+    case board.winning_marker
     when human.marker
       puts "You won!"
     when computer.marker
@@ -163,11 +167,23 @@ class TTTGame
     system 'clear'
   end
 
+  def reset
+    board.reset
+    clear
+  end
+
+  def display_play_again_message
+    puts "Let's play again!"
+    puts ' '
+  end
+
   def play
     clear
     display_welcome_message
+
     loop do
-      display_board(false)
+      display_board
+
       loop do
         human_moves
         break if board.someone_won? || board.full?
@@ -175,16 +191,14 @@ class TTTGame
         computer_moves
         break if board.someone_won? || board.full?
 
-        display_board
+        clear_screen_and_display_board
       end
       display_result
       break unless play_again?
-
-      board.reset
-      clear
-      puts "Let's play again!"
-      puts ' '
+      reset
+      display_play_again_message
     end
+
     display_goodbye_message
   end
 end
